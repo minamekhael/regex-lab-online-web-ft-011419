647f15b1a27a2b3f244e6dc6d8da4f5e51f3d0e5
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-19 19:52:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/\)
+  if text.match(/\A[A-Z]\w*\W\b/)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
